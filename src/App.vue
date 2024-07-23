ba16acc8719068597195b691862483dd140e4757
GUO YANKE <hi@yankeguo.com>
2024-07-23 11:32:19 +0800

    feat: add walletconnect

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,34 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/vue";
+
+import { gnosis } from "viem/chains";
+import { reconnect } from "@wagmi/core";
+
+const projectId = "a43f0c40b9d631837c164d361959ace2";
+
+const metadata = {
+  name: "Permlog",
+  description: "Permanent log on the blockchain",
+  url: "https://permlog.yankeguo.com",
+  icons: ["https://permlog.yankeguo.com/favicon.ico"],
+};
+
+const wagmiConfig = defaultWagmiConfig({
+  chains: [gnosis],
+  projectId,
+  metadata,
+});
+
+reconnect(wagmiConfig);
+
+createWeb3Modal({
+  wagmiConfig,
+  projectId,
+  enableAnalytics: true,
+  enableOnramp: true,
+});
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <w3m-button />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
