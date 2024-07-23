cfbb8582fcb895970fff4b7d2c1dee51ca1ea618
GUO YANKE <hi@yankeguo.com>
2024-07-23 13:02:37 +0800

    refactor: refine primevue

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ import "./assets/style.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import PrimeVue from "primevue/config";
-import Lara from "./presets/lara";
+
+// @ts-ignore
+import pt from "./presets/permlog";
 
 const app = createApp(App);
 
 app.use(PrimeVue, {
   unstyled: true,
-  pt: Lara,
+  pt,
 });
 
 app.mount("#app");
---
+++ src/presets/permlog/index.js
@@ -0,0 +1,11 @@
+import global from "./global.js";
+import button from "./button";
+import inputtext from "./inputtext";
+import textarea from "./textarea";
+
+export default {
+  global,
+  button,
+  inputtext,
+  textarea,
+};
